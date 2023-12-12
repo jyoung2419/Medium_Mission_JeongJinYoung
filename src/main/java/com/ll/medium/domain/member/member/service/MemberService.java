0232d8c8b93bbb1197cc9b3bb622891d87bb0d0b
Jeong jinyoung <jjyoung2419@gmail.com>
2023-12-12 22:31:35 +0900

    joinForm

---
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.ll.medium.domain.member.member.service;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Member create(String username, String email, String password) {
+        Member member = new Member();
+        member.setUsername(username);
+        member.setEmail(email);
+        member.setPassword(passwordEncoder.encode(password));
+        this.memberRepository.save(member);
+        return member;
+    }
+}
--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +25,10 @@ public class SecurityConfig {
             csrf.ignoringRequestMatchers(new String[]{"/h2-console/**"});
         });
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
