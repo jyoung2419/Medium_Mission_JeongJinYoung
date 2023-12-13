a6750eb836e910c187913230a6c0f34f0becc915
Jeong jinyoung <jjyoung2419@gmail.com>
2023-12-13 20:19:16 +0900

    RsData

--- src/main/java/com/ll/medium/global/rq/Rq/Rq.java
+++ src/main/java/com/ll/medium/global/rq/Rq/Rq.java
@@ -1,5 +1,6 @@
 package com.ll.medium.global.rq.Rq;
 
+import com.ll.medium.global.rsData.RsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,5 +31,17 @@ public class Rq {
         }
 
         return sb.toString();
+    }
+
+    public String historyBack(String msg) {
+        request.setAttribute("failMsg", msg);
+
+        return "global/js";
+    }
+
+    public String redirectOrBack(RsData<?> rs, String path) {
+        if (rs.isFail()) return historyBack(rs.getMsg());
+
+        return redirect(path, rs.getMsg());
     }
 }
