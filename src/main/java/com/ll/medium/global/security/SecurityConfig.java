c75635ab1728b67237ff4e72676f200f95c77044
Jeong jinyoung <jjyoung2419@gmail.com>
2023-12-13 21:02:35 +0900

    SecurityConfig 수정

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -3,32 +3,43 @@ package com.ll.medium.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class SecurityConfig {
-    public SecurityConfig() {
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((authorizeRequests) -> {
-            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)authorizeRequests.requestMatchers(new String[]{"/**"})).permitAll();
-        }).headers((headers) -> {
-            headers.frameOptions((frameOptions) -> {
-                frameOptions.sameOrigin();
-            });
-        }).csrf((csrf) -> {
-            csrf.ignoringRequestMatchers(new String[]{"/h2-console/**"});
-        });
-        return (SecurityFilterChain)http.build();
-    }
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/**")
+                                .permitAll()
+                )
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                )
+                .csrf(
+                        csrf ->
+                                csrf.ignoringRequestMatchers(
+                                        "/h2-console/**"
+                                )
+                )
+                .formLogin(
+                        formLogin ->
+                                formLogin
+                                        .loginPage("/member/login")
+                                        .loginProcessingUrl("/member/login")
+                                        .usernameParameter("username")
+                                        .passwordParameter("password")
+                                        .defaultSuccessUrl("/?msg=" + URLEncoder.encode("환영합니다.", StandardCharsets.UTF_8))
+                );
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return http.build();
     }
 }
