6a3ddfce194adf43332aff4627b7235e8da59bf5
Jeong jinyoung <jjyoung2419@gmail.com>
2023-12-21 20:09:51 +0900

    findByIsPublishedAndTitleContainingIgnoreCaseOrIsPublishedAndBodyContainingIgnoreCase

--- src/main/java/com/ll/medium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/post/service/PostService.java
@@ -38,6 +38,4 @@ public class PostService {
     }
 
     public Page<Post> search(String kw, Pageable pageable) {
-        return postRepository.findByTitleContainingIgnoreCaseOrBodyContainingIgnoreCase(kw, kw, pageable);
-    }
-}
+        return postRepository.findByIsPublishedAndTitleContainingIgnoreCaseOrIsPublishedAndBodyContainingIgnoreCase(true, kw, true, kw, pageable);}
